4541f9edc94d8c868950fec096118bb09df44b90
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2025-08-24 20:05:23 +0530

    enhance: handle recursive dom node creation in createDomElement

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from "./core/hooks/useState";
 export const App = () => {
     console.log('running/ building app')
     const [count, setCount] = useState(0);
+    const [count1, setCount1] = useState(0);
     const [text, setText] = useState("Hello");
 
     const title = h(
@@ -15,9 +16,11 @@ export const App = () => {
     );
 
     // const button = createElement('button', {}, `Count`)
-    const button = h('button', {onClick: () => setCount(count + 1)}, `Count ${count}`)
+    const button = h('button', {onClick: () => setCount(count + 1)}, `Count ${count}`);
+    const button1 = h('button', {onClick: () => setCount1(count + 1)}, `Count ${count1}`);
+    const div = h('div', {}, button, button1);
 
     // return h('div', {}, title, button)
     // return createElement('div', {}, button)
-    return button;
+    return div;
 }
--- src/core/createDomElement.js
+++ src/core/createDomElement.js
@@ -1,22 +1,31 @@
 export default function createDomElement(vnode) {
     const { type, props, children } = vnode;
 
-    const el = document.createElement(type);
-
-    for (const [key, value] of Object.entries(props || {})) {
-        if (key.startsWith('on') && typeof value === 'function') {
-            const event = key.slice(2).toLowerCase();
-            el.addEventListener(event, value);
-        }
+    if (typeof vnode === 'string' || typeof vnode === 'number') {
+        return document.createTextNode(vnode);
     }
 
-    children.flat().forEach(child => {
-        if (typeof child === 'string' || typeof child === 'number') {
-            el.appendChild(document.createTextNode(child));
-        } else if (child instanceof Node) {
-            el.appendChild(child);
-        }
-    });
+    if (typeof vnode === 'object' && vnode !== null) {
 
-    return el;
+        const el = document.createElement(type);
+
+        for (const [key, value] of Object.entries(props || {})) {
+            if (key.startsWith('on') && typeof value === 'function') {
+                const event = key.slice(2).toLowerCase();
+                el.addEventListener(event, value);
+            }
+        }
+
+        children.flat().forEach((child) => {
+            if (typeof child.type === 'string') {
+                const childElement = createDomElement(child);
+                childElement && el.appendChild(childElement);
+            } else if (typeof child === 'string' || typeof child === 'number') {
+                const textNode = document.createTextNode(child);
+                textNode && el.appendChild(textNode);
+            }
+        });
+
+        return el;
+    }
 }
