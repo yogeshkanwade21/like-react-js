8d53ef53a1a074b1936d94a95ede4bb568166918
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2025-08-25 16:27:58 +0530

    add: counter component to example app

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,24 +1,36 @@
+/** @jsx h */
 import { h, useState } from "../../src/public-api";
+
+import Counter from "./components/Counter";
 
 const App = () => {
     console.log('running/ building app')
     const [count, setCount] = useState(0);
-    const [count1, setCount1] = useState(0);
-    const [text, setText] = useState("Hello");
+    // const [count1, setCount1] = useState(0);
+    // const [text, setText] = useState("Hello");
 
-    const title = h(
-        'h1',
-        {onClick: () => {
-            text === 'Hello' ? setText("World") : setText("Hello")
-        }},
-        text
-    );
+    // const title = h(
+    //     'h1',
+    //     {onClick: () => {
+    //         text === 'Hello' ? setText("World") : setText("Hello")
+    //     }},
+    //     text
+    // );
 
-    const button = h('button', {onClick: () => setCount(count + 1)}, `Count ${count}`);
-    const button1 = h('button', {onClick: () => setCount1(count1 + 1)}, `Count ${count1}`);
-    const div = h('div', {}, button, button1, title);
+    // const button = h('button', {onClick: () => setCount(count + 1)}, `Count ${count}`);
+    // const button1 = h('button', {onClick: () => setCount1(count1 + 1)}, `Count ${count1}`);
+    // const div = h('div', {}, button, button1, title);
 
-    return div
+    return (
+      <div>
+      
+        <Counter />
+        <Counter />
+      </div>
+    )
+    // return <button onClick={() => setCount(count + 1)}>Count {count}</button>
+    
+    
 }
 
 export default App;
