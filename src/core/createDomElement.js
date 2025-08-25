b01a372bdf7b11160e0c9a1f9d5aa031aef9da7f
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2025-08-25 16:27:16 +0530

    enhance: handle vnode.type === function

--- src/core/createDomElement.js
+++ src/core/createDomElement.js
@@ -1,14 +1,20 @@
 export default function createDomElement(vnode) {
-    const { type, props, children } = vnode;
-
+    
     if (typeof vnode === 'string' || typeof vnode === 'number') {
         return document.createTextNode(vnode);
     }
-
+    
     if (typeof vnode === 'object' && vnode !== null) {
+        const { type, props = {}, children = [] } = vnode;
+
+        if (typeof type === 'function') {
+            const componentVnode = type(props || {});
+            return createDomElement(componentVnode);
+        }
 
         const el = document.createElement(type);
 
+        // for props
         for (const [key, value] of Object.entries(props || {})) {
             if (key.startsWith('on') && typeof value === 'function') {
                 const event = key.slice(2).toLowerCase();
@@ -16,13 +22,18 @@ export default function createDomElement(vnode) {
             }
         }
 
+        // for children
         children.flat().forEach((child) => {
             if (typeof child.type === 'string') {
-                const childElement = createDomElement(child);
-                childElement && el.appendChild(childElement);
+                const childDomNode = createDomElement(child);
+                childDomNode instanceof Node && el.appendChild(childDomNode);
+            } else if (typeof child.type === 'function') {
+                const childVnode = child.type(child.props || {});
+                const childDomNode = createDomElement(childVnode);
+                childDomNode instanceof Node && el.appendChild(childDomNode);
             } else if (typeof child === 'string' || typeof child === 'number') {
                 const textNode = document.createTextNode(child);
-                textNode && el.appendChild(textNode);
+                textNode instanceof Node && el.appendChild(textNode);
             }
         });
 
