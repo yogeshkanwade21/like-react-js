6aef44f9cb7d4103d74ea92ace5614d7d2046499
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2025-08-25 21:46:58 +0530

    enhance createDomElement: handle props (input value, disabled and className)

--- src/core/createDomElement.js
+++ src/core/createDomElement.js
@@ -16,9 +16,31 @@ export default function createDomElement(vnode) {
 
         // for props
         for (const [key, value] of Object.entries(props || {})) {
+            // event handlers
             if (key.startsWith('on') && typeof value === 'function') {
                 const event = key.slice(2).toLowerCase();
                 el.addEventListener(event, value);
+            }
+            // value of controlled inputs
+            else if (key === 'value' && (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA')) {
+                el.value = value;
+            }
+            // Handle boolean attributes like 'disabled'
+            // A disabled button should be disabled when the value is a truthy value.
+            else if (typeof value === 'boolean') {
+                if (value) {
+                    el.setAttribute(key, '');
+                } else {
+                    el.removeAttribute(key);
+                }
+            }
+            // handle className (utility classes)
+            else if (key === 'className') {
+                el.setAttribute('class', value);
+            }
+            // set other standard attributes
+            else {
+                el.setAttribute(key, value);
             }
         }
 
