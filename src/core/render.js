7df21e6712fec946f8c86fc30f5b709843cbda26
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2025-09-02 19:39:37 +0530

    enhance: render
    call patch function instead of .replaceWith() on rerenders

--- src/core/render.js
+++ src/core/render.js
@@ -1,36 +1,32 @@
 import { createInstance, resetInstance, setCurrentInstance } from "./instanceManager";
 import createDomElement from "./createDomElement";
 import h from './h';
+import patch from "./patch";
 
 export default function render(componentFunction, container) {
 
   // Create the initial VNode for the root component
   const rootVNode = h(componentFunction);
-  console.log('got rootVNode', rootVNode)
+
   // Create a unique instance for the root component
   const instance = createInstance(rootVNode);
+  let oldVNode;
 
-  const rerenderFunction = () => {
+  const rerenderFunction = () => {    
     resetInstance(instance);
     setCurrentInstance(instance);
     
     const vnode = instance.componentFunction(rootVNode.props);
-    // then give this as input to createDomElement
-    const dom = createDomElement(vnode);
-    console.log('built dom', dom)
-
-    if (instance.dom) {
-      instance.dom.replaceWith(dom);
-    } else {
-      container.innerHTML = '';
-      container.appendChild(dom);
-    }
-    instance.dom = dom;
+    patch(oldVNode, vnode);
   };
 
   instance.rerenderFunction = rerenderFunction;
+  setCurrentInstance(instance);
 
-  console.log('before calling rerenderFunction')
-  rerenderFunction();
-  console.log('after calling rerenderFunction')
+  const vnode = instance.componentFunction(rootVNode.props);
+  const dom = createDomElement(vnode);
+  oldVNode = vnode;
+  
+  container.innerHTML = '';
+  container.appendChild(dom);
 }
